9e67ed30b13feb1fbfbbef39d57c6b3f8afd9a64
edm-r <edmondyopa22@gmail.com>
2026-02-03 02:05:56 +0100

    Feat: Ajout de la vue update une tache

--- taches/urls.py
+++ taches/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.liste_taches, name='liste_taches'),
     path('ajouter/', views.ajouter_tache, name='ajouter_tache'),
-    # path('modifier/<int:tache_id>/', views.modifier_tache, name='modifier_tache'),
+    path('modifier/<int:tache_id>/', views.modifier_tache, name='modifier_tache'),
     # path('supprimer/<int:tache_id>/', views.supprimer_tache, name='supprimer_tache'),
     # path('basculer_termine/<int:tache_id>/', views.basculer_termine, name='basculer_termine'),
 ]
--- taches/views.py
+++ taches/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Tache
 from .forms import TacheForm
@@ -23,4 +23,21 @@ def ajouter_tache(request):
         form = TacheForm()
     
     context = {'form': form, 'titre_page': 'Ajouter une tâche'}
+    return render(request, 'taches/tache_form.html', context)
+
+
+def modifier_tache(request, tache_id):
+    """Modifie une tâche existante via un formulaire."""
+    tache = get_object_or_404(Tache, pk=tache_id)
+    
+    if request.method == 'POST':
+        form = TacheForm(request.POST, instance=tache)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tâche modifiée avec succès!')
+            return redirect('liste_taches')
+    else:
+        form = TacheForm(instance=tache)
+    
+    context = {'form': form, 'titre_page': 'Modifier une tâche'}
     return render(request, 'taches/tache_form.html', context)
